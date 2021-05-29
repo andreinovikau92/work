cac606f8f83443aea99cac27999b80cb798e1a87
Andrei <example@gmail.com>
2021-05-29 11:13:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+ 
+let number = 5;
+const leftBorderWith = 1; 
+
+/* number = 10;
+alert(number); */
+
+{
+    let result = 12;
+}
+
+/* console.log(result); */
+
+/* alert(5);
+[].push('a'); */
+
+console.log(-4/0);
+console.log('string'*9);
+
+const persone = "Alex";
+
+const bool = true;
+
+/* console.log(something); */
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+/* console.log(obj.name); */
+console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+/* alert('Hello'); */
+
+/* const result = confirm('Вам ёсьць 18?');
+console.log(result); */
+
+/* const answer = +prompt('Вам ёсьць 18?', '18');
+console.log(answer + 15); */
+
+/* const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваше фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers); */
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
